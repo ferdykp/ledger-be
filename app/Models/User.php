c65f031ff99d0a7e981852ab0b134c5c19040809
ferdy <kpferdy@gmail.com>
2026-09-18 15:12:11 +0700

    merge

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Storage;
 use Laravel\Sanctum\HasApiTokens;
-<<<<<<< HEAD
-=======
 use App\Notifications\ResetPasswordNotification;
->>>>>>> 849c666 (new)
 
 class User extends Authenticatable
 {
@@ -33,14 +30,11 @@ class User extends Authenticatable
         return 'https://ui-avatars.com/api/?name=' . urlencode($this->name) . '&background=6C4CF1&color=fff';
     }
 
-<<<<<<< HEAD
-=======
     public function sendPasswordResetNotification($token): void
     {
         $this->notify(new ResetPasswordNotification($token));
     }
 
->>>>>>> 849c666 (new)
     public function accounts()
     {
         return $this->hasMany(Account::class);
